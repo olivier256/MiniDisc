1d880cf7b69f07d57b174981aaa60684aa7e1e16
Olivier <olivier.brosseron@icloud.com>
2026-02-13 12:51:57 +0100

    Comment on sound group length and cluster size

--- src/minidisc/MiniDiscDiscType.java
+++ src/minidisc/MiniDiscDiscType.java
@@ -1,17 +1,17 @@
 package minidisc;
 
-/**
- * 1 sound group = 11.6ms
- * 11 sound groups = 2 secteurs audio
- * 1 cluster
- * = 32 secteurs audio
- * = 16 x 2 secteurs audio
- * = 16 x 11 sound groups
- * = 176 sound groups
- * = 2,0416ms
- *
- * @see <A href="https://www.minidisc.org/French_tech/section4.html">Structure des données sur le disque</A>
- */
+    /**
+     *
+    1 sound group = 424B stereo = 512 samples @ 44.1kHz ~ 11.6ms / sample
+    11 sound groups = 2 secteurs audio
+    1 cluster
+    = 32 secteurs audio
+    = 16 x 2 secteurs audio
+    = 16 x 11 sound groups
+    = 176 sound groups
+    = 2,04335601ms
+     @see <A href="https://www.minidisc.org/French_tech/section4.html">Structure des données sur le disque</A>
+     */
 public enum MiniDiscDiscType {
     MD60(1764),
     /*
@@ -31,4 +31,11 @@ public enum MiniDiscDiscType {
         return programClusters;
     }
 
+    public int programEndExclusive() {
+        return MiniDiscLayout.PROGRAM_START + programClusters;
+    }
+
+    public int totalClusters() {
+        return programEndExclusive() + MiniDiscLayout.LEAD_OUT_CLUSTERS;
+    }
 }
